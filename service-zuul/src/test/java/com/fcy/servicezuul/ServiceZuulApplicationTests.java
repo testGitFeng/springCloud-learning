72887ead44305938002db1acb55ac04fcd20c2ca
testGitFeng <51194255+testGitFeng@users.noreply.github.com>
2019-11-12 15:48:45 +0800

    Add files via upload

---
+++ service-zuul/src/test/java/com/fcy/servicezuul/ServiceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcy.servicezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
